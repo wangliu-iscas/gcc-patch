cc9031f8711511485db5032729ed8703c5f49a94
Arsen Arsenović <arsen@aarsen.me>
2022-09-30 18:45:51 +0200

    c-family: Implement new `int main' semantics in freestanding
    From now, by default, (specifically) `int main' in freestanding will
    implicitly return 0, as it does for hosted modes. The old behaviour is
    still accessible via -fno-builtin-main.
    
    gcc/c-family/ChangeLog:
    
    	* c-common.cc (disable_builtin_function): Support special value
    	`main' that, in freestanding, allows disabling special casing
    	placed around `main'.
    	* c-common.h: Add flag_builtin_main.
    	(want_builtin_main_p): New function, true iff hosted OR
    	builtin_main are set.
    
    gcc/c/ChangeLog:
    
    	* c-decl.cc (grokdeclarator): Consider flag_builtin_main when
    	deciding whether to emit warnings.
    	(finish_function): Consider flag_builtin_main when deciding
    	whether to emit an implicit zero return.
    	* c-objc-common.cc (c_missing_noreturn_ok_p): Consider missing
    	noreturn okay only when hosted or when builtin_main is enabled.
    
    gcc/cp/ChangeLog:
    
    	* cp-tree.h (DECL_MAIN_P): Consider flag_builtin_main when
    	deciding whether this function is to be the special function
    	main.
    
    gcc/ChangeLog:
    
    	* doc/invoke.texi: Document -fno-builtin-main.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/c11-noreturn-4.c: Add -fno-builtin-main to options.
    	* gcc.dg/inline-10.c: Likewise.
    	* gcc.dg/noreturn-4.c: Likewise.
    	* g++.dg/freestanding-main-implicitly-returns.C: New test.
    	* g++.dg/no-builtin-main.C: New test.
    	* gcc.dg/freestanding-main-implicitly-returns.c: New test.
    	* gcc.dg/no-builtin-main.c: New test.
    
    Signed-off-by: Arsen Arsenović <arsen@aarsen.me>

--- gcc/testsuite/gcc.dg/noreturn-4.c
+++ gcc/testsuite/gcc.dg/noreturn-4.c
@@ -1,6 +1,6 @@
 /* Check for "noreturn" warning in main. */
 /* { dg-do compile } */
-/* { dg-options "-O2 -Wmissing-noreturn -ffreestanding" } */
+/* { dg-options "-O2 -Wmissing-noreturn -ffreestanding -fno-builtin-main" } */
 extern void exit (int) __attribute__ ((__noreturn__));
 
 int
