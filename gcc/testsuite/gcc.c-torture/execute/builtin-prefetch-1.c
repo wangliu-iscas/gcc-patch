e3d34eaca029257b58e0df5a1916fdc09b3054a3
Haochen Jiang <haochen.jiang@intel.com>
2022-10-14 16:34:05 +0800

    Add a parameter for the builtin function of prefetch to align with LLVM
    gcc/ChangeLog:
    
    	* builtins.cc (expand_builtin_prefetch): Handle the fourth parameter in
    	expand function.
    	* config/aarch64/aarch64-sve.md: Add default parameter value.
    	* config/aarch64/aarch64.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/alpha/alpha.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/arc/arc.md: Add default parameter value.
    	* config/arm/arm.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/frv/frv.md: Ditto.
    	* config/i386/i386.md: Ditto.
    	* config/ia64/ia64.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/mips/mips.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/pa/pa.md: Ditto.
    	* config/rs6000/rs6000.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/s390/s390.cc (s390_expand_cpymem): Generate fourth parameter for
    	gen_prefetch call.
    	(s390_expand_setmem): Ditto.
    	(s390_expand_cmpmem): Ditto.
    	* config/s390/s390.md (prefetch): New define_expand.
    	(*prefetch): Add default parameter value.
    	* config/sh/sh.md: Ditto.
    	* config/sparc/sparc.md: Ditto.
    	* doc/rtl.texi: Document cache variable for prefetch.
    	* rtl.def (PREFETCH): Change prefetch DEF_RTL_EXPR to add fourth parameter.
    	* rtlanal.cc (setup_reg_subrtx_bounds): Change gcc_checking_assert for
    	fourth parameter.
    	* target-insns.def (prefetch): Add fourth rtx for prefetch.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.c-torture/execute/builtin-prefetch-1.c: Add fourth parameter for
    	testcases.
    	* gcc.c-torture/execute/builtin-prefetch-2.c: Ditto.
    	* gcc.c-torture/execute/builtin-prefetch-3.c: Ditto.
    	* gcc.c-torture/execute/builtin-prefetch-4.c: Ditto.
    	* gcc.c-torture/execute/builtin-prefetch-5.c: Ditto.
    	* gcc.c-torture/execute/builtin-prefetch-6.c: Ditto.
    	* gcc.dg/builtin-prefetch-1.c: Ditto.
    	* gcc.misc-tests/i386-pf-3dnow-1.c: Ditto.
    	* gcc.misc-tests/i386-pf-athlon-1.c: Ditto.
    	* gcc.misc-tests/i386-pf-none-1.c: Ditto.
    	* gcc.misc-tests/i386-pf-sse-1.c: Ditto.
    	* gcc.target/i386/avx-1.c: Change prefetch macro define to variable args.
    	* gcc.target/i386/sse-13.c: Ditto.
    	* gcc.target/i386/sse-23.c: Ditto.
    	* gcc.target/aarch64/prefetchi-1.c: New test.
    	* gcc.target/alpha/prefetchi-1.c: Ditto.
    	* gcc.target/arc/prefetchi-1.c: Ditto.
    	* gcc.target/arm/prefetchi-1.c: Ditto.
    	* gcc.target/hppa/prefetchi-1.c: Ditto.
    	* gcc.target/i386/prefetchi-1.c: Ditto.
    	* gcc.target/ia64/prefetchi-1.c: Ditto.
    	* gcc.target/mips/prefetchi-1.c: Ditto.
    	* gcc.target/powerpc/prefetchi-1.c: Ditto.
    	* gcc.target/s390/prefetchi-1.c: Ditto.
    	* gcc.target/sh/prefetchi-1.c: Ditto.
    	* gcc.target/sparc/prefetchi-1.c: Ditto.

--- gcc/testsuite/gcc.c-torture/execute/builtin-prefetch-1.c
+++ gcc/testsuite/gcc.c-torture/execute/builtin-prefetch-1.c
@@ -1,57 +1,62 @@
 /* Test that __builtin_prefetch does no harm.
 
-   Prefetch using all valid combinations of rw and locality values.
+   Prefetch using all valid combinations of cache, rw and locality values.
    These must be compile-time constants.  */
 
 #define NO_TEMPORAL_LOCALITY 0
 #define LOW_TEMPORAL_LOCALITY 1
-#define MODERATE_TEMPORAL_LOCALITY 1
+#define MODERATE_TEMPORAL_LOCALITY 2
 #define HIGH_TEMPORAL_LOCALITY 3
 
 #define WRITE_ACCESS 1
 #define READ_ACCESS 0
 
+#define DATA_PRFCH 1
+#define INST_PRFCH 0
+
 enum locality { none, low, moderate, high };
 enum rw { read, write };
+enum cache { inst, data };
 
 int arr[10];
 
 void
 good_const (const int *p)
 {
-  __builtin_prefetch (p, 0, 0);
-  __builtin_prefetch (p, 0, 1);
-  __builtin_prefetch (p, 0, 2);
-  __builtin_prefetch (p, READ_ACCESS, 3);
-  __builtin_prefetch (p, 1, NO_TEMPORAL_LOCALITY);
-  __builtin_prefetch (p, 1, LOW_TEMPORAL_LOCALITY);
-  __builtin_prefetch (p, 1, MODERATE_TEMPORAL_LOCALITY);
-  __builtin_prefetch (p, WRITE_ACCESS, HIGH_TEMPORAL_LOCALITY);
+  __builtin_prefetch (p, 0, 0, 1);
+  __builtin_prefetch (p, 0, 1, 1);
+  __builtin_prefetch (p, 0, 2, 1);
+  __builtin_prefetch (p, READ_ACCESS, 3, 1);
+  __builtin_prefetch (p, 1, NO_TEMPORAL_LOCALITY, 1);
+  __builtin_prefetch (p, 1, LOW_TEMPORAL_LOCALITY, 1);
+  __builtin_prefetch (p, 1, MODERATE_TEMPORAL_LOCALITY, 1);
+  __builtin_prefetch (p, WRITE_ACCESS, HIGH_TEMPORAL_LOCALITY, DATA_PRFCH);
 }
 
 void
 good_enum (const int *p)
 {
-    __builtin_prefetch (p, read, none);
-    __builtin_prefetch (p, read, low);
-    __builtin_prefetch (p, read, moderate);
-    __builtin_prefetch (p, read, high);
-    __builtin_prefetch (p, write, none);
-    __builtin_prefetch (p, write, low);
-    __builtin_prefetch (p, write, moderate);
-    __builtin_prefetch (p, write, high);
+    __builtin_prefetch (p, read, none, data);
+    __builtin_prefetch (p, read, low, data);
+    __builtin_prefetch (p, read, moderate, data);
+    __builtin_prefetch (p, read, high, data);
+    __builtin_prefetch (p, write, none, data);
+    __builtin_prefetch (p, write, low, data);
+    __builtin_prefetch (p, write, moderate, data);
+    __builtin_prefetch (p, write, high, data);
 }
 
 void
 good_expr (const int *p)
 {
-  __builtin_prefetch (p, 1 - 1, 6 - (2 * 3));
-  __builtin_prefetch (p, 1 + 0, 1 + 2);
+  __builtin_prefetch (p, 1 - 1, 6 - (2 * 3), 1 + 0);
+  __builtin_prefetch (p, 1 + 0, 1 + 2, 0 + 1);
 }
 
 void
 good_vararg (const int *p)
 {
+  __builtin_prefetch (p, 0, 3, 1);
   __builtin_prefetch (p, 0, 3);
   __builtin_prefetch (p, 0);
   __builtin_prefetch (p, 1);
--- gcc/testsuite/gcc.misc-tests/i386-pf-sse-1.c
+++ gcc/testsuite/gcc.misc-tests/i386-pf-sse-1.c
@@ -9,14 +9,14 @@ char *msg = "howdy there";
 
 void foo (char *p)
 {
-  __builtin_prefetch (p, 0, 0);
-  __builtin_prefetch (p, 0, 1);
-  __builtin_prefetch (p, 0, 2);
-  __builtin_prefetch (p, 0, 3);
-  __builtin_prefetch (p, 1, 0);
-  __builtin_prefetch (p, 1, 1);
-  __builtin_prefetch (p, 1, 2);
-  __builtin_prefetch (p, 1, 3);
+  __builtin_prefetch (p, 0, 0, 1);
+  __builtin_prefetch (p, 0, 1, 1);
+  __builtin_prefetch (p, 0, 2, 1);
+  __builtin_prefetch (p, 0, 3, 1);
+  __builtin_prefetch (p, 1, 0, 1);
+  __builtin_prefetch (p, 1, 1, 1);
+  __builtin_prefetch (p, 1, 2, 1);
+  __builtin_prefetch (p, 1, 3, 1);
 }
 
 int main ()
